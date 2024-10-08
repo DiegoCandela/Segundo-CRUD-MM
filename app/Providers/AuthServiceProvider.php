28a7d906b1697d6926cd19949f7206492c669662
diegocandela <diegocandela0128@gmail.com>
2024-10-08 11:38:16 -0500

    Ultimo_commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,19 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\cliente;
+use App\Models\compra;
+use App\Models\producto;
+use App\Models\proveedor;
+use App\Models\User;
+use App\Models\venta;
+use App\Policies\ClientePolicy;
+use App\Policies\CompraPolicy;
+use App\Policies\ProductoPolicy;
+use App\Policies\ProveedorPolicy;
+use App\Policies\UserPolicy;
+use App\Policies\VentaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +26,12 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        cliente::class => ClientePolicy::class,
+        producto::class => ProductoPolicy::class,
+        compra::class => CompraPolicy::class,
+        proveedor::class => ProveedorPolicy::class,
+        venta::class => VentaPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
